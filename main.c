cf0c5c772e341be1e711e5813bfa8a01c0fe8e3c
UltGab04 <gabriel.workDev.UNI2004@gmail.com>
2025-01-29 19:20:59 -0300

    Testando

---
+++ main.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+
+int main()
+{
+    setlocale(LC_ALL,"Portuguese");
+    float num=0,num2=0, res=1;
+    int op;
+    int enter = 1;
+
+    printf("\nDigite o número: ");
+    scanf(" %f", &res);
+
+    do{
+        printf("\nDigite a operação \n 1-soma  2-Subtração\n 4-Divisão  3-Multiplicação\n");
+        scanf(" %d", &op);
+        printf("\nDigite o número: ");
+        scanf(" %f", &num2);
+        switch(op) {
+        case 1:
+            res +=num2;
+            break;
+        case 2:
+            res -=num2;
+            break;
+        case 3:
+            res *=num2;
+            break;
+        case 4:
+            res /= num2;
+            break;
+        default:
+            printf("Op inválido!");
+            break;
+        }
+        printf("\nResultado parcial: %.2f", res);
+        printf("\nDigite 0 para calcular: ");
+        scanf(" %d", &enter);
+    } while(enter != 0);
+
+    printf("Resultado: %.2f", res);
+
+    return 0;
+}
